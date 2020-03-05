727c4ce11e898de5ab236ccc18b94dc9c851c1c2
Kent Riemondy <kent.riemondy@gmail.com>
2020-03-05 10:01:30 -0700

    update markdowns

--- R/utils.R
+++ R/utils.R
@@ -64,3 +64,54 @@ top_marker_matrix <-  function(mkrs,
 
   res
 }
+
+
+get_marker_summaries <- function(so,
+                             group_col = "seurat_clusters",
+                             outdir = "./",
+                             prefix = NULL,
+                             min_pct = 0.10,
+                             pvalue = 0.05,
+                             only_pos = TRUE,
+                             tsv_output_dir = NULL,
+                             xlsx_output_dir = NULL){
+
+  full_markers <- presto::wilcoxauc(so, group_col)
+
+  mkrs <- filter(full_markers, padj < pvalue,  pct_in > min_pct)
+  if(only_pos){
+    mkrs <- filter(mkrs, logFC > 0)
+  }
+  mkrs <- mkrs %>%
+    group_by(group) %>%
+    arrange(padj,
+            desc(logFC),
+            .by_group = TRUE)
+
+  if(is.null(prefix)){
+    prefix <- ""
+  } else {
+    prefix <- str_c(prefix, "_")
+  }
+
+  if(is.null(tsv_output_dir)){
+    tsv_output_dir <- outdir
+  }
+
+  if(is.null(xlsx_output_dir)){
+    xlsx_output_dir <- outdir
+  }
+
+  mkrs %>%
+    write_tsv(file.path(tsv_output_dir,
+                        str_c(prefix, "cluster_markers.tsv")))
+
+  mkrs %>%
+    ungroup() %>%
+    split(., .$group) %>%
+    write_markers_xlsx(.,
+                       file.path(xlsx_output_dir,
+                                 str_c(prefix, "cluster_markers.xlsx")))
+
+  dplyr::lst(full_markers, mkrs)
+}
